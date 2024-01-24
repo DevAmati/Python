0d4bc262ecf98f8e6ee8116e651c260efec147c8
DevAmati <morganamati122@gmail.com>
2024-01-24 20:13:21 +0300

    Assignment 1

---
+++ Grooss_pay.py
@@ -0,0 +1,8 @@
+#Prompt user to input the number of hours
+Hours = int(input("Enter the number of hours:"))
+#Prompt user to input the rate per hour
+Rate = float(input("Enter the rate per hour:"))
+#Declaring the formula for calculating the Gross Pay
+Gross_pay =Hours*Rate
+#Display the calculated Gross Pay
+print(Gross_pay)
---
+++ volume.py
@@ -0,0 +1,9 @@
+
+#Declaration of PI
+PI: float = 3.142
+#Prompts user to input the radius for the sphere
+radius = int(input("Enter the Radius:"))
+#Formula for finding the volume
+volume = (4/3)*PI*radius*radius*radius
+#Display the volume
+print(volume)
